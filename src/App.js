07be0eab138898acc585fe3427f462b2734d79d3
Agata <agata@swistowski.org>
2021-03-07 12:23:11 +0000

    Use Material-ui to display list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import React, {useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
 
 import './App.css';
 import {giveMeMyBooks} from "./actions";
+
+function Publication({book}) {
+    return (
+        <Typography
+            component="span"
+            variant="body2"
+            color="textPrimary"
+        >
+            Published:
+            {book.book_publication_year}, {book.book_publication_city} {book.book_publication_country}
+        </Typography>
+    )
+}
+
+function BookDetails({book}) {
+    return (
+        <>
+            <Typography
+                component="span"
+                variant="h5"
+                color="textPrimary"
+            >
+            {book.book_author}
+            </Typography>
+            <br />
+            <Publication book={book}/>
+            <Typography component="span"
+                variant="body2"
+                color="textPrimary"
+            >
+                <br />
+                Pages:
+                {book.book_pages}
+            </Typography>
+        </>
+    )
+}
 
 function App() {
 
@@ -13,16 +55,15 @@ function App() {
         dispatch(giveMeMyBooks())
     }, [dispatch])
 
-
     return (
         <div className="App">
-            <ul>
+            <List>
                 {books.map(item => (
-                    <li key={item.id}>
-                        <p>{item.book_author}</p>
-                    </li>
+                    <ListItem key={item.id}>
+                        <ListItemText primary={item.book_title} secondary={<BookDetails book={item}/>}/>
+                    </ListItem>
                 ))}
-            </ul>
+            </List>
         </div>
     );
 }
